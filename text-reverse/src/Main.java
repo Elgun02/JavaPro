398c38844ada80c68206b675189534fc95d3f2ae
Elgun <eabdulaev12@mail.ru>
2023-10-11 04:05:20 +0300

    commit

--- text-reverse/src/Main.java
+++ text-reverse/src/Main.java
@@ -1,17 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Stack;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void main (String[]args){
+        String str = "Привет, мир";
+        reverse(str);
+    }
+    public static void reverse(String str) {
+        str = str.toLowerCase();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+            if (stack.size() == 5 || i == str.length() - 1) {
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop());
+                }
+            }
         }
     }
 }
